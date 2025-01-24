ec042561cbcb55e753733fcc570f7cea2231bbed
Deanna H <deannamaharaj@gmail.com>
2025-01-24 16:31:35 -0600

    Add VacationList activity to UI package

--- app/src/main/java/com/example/vacationplanner/UI/VacationList.java
+++ app/src/main/java/com/example/vacationplanner/UI/VacationList.java
@@ -1,6 +1,11 @@
 package com.example.vacationplanner.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.vacationplanner.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class VacationList extends AppCompatActivity {
 
@@ -17,6 +23,16 @@ public class VacationList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_vacation_list);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VacationList.this, VacationDetails.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,5 +40,28 @@ public class VacationList extends AppCompatActivity {
         });
 
         System.out.println(getIntent().getStringExtra("test"));
+
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_vacation_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId()==R.id.sample){
+            Toast.makeText(VacationList.this, "put in sample data", Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if(item.getItemId()==android.R.id.home){ // for a back button
+            this.finish();
+            // Comment out the previous line and use the next two lines instead for back button to go to VacationDetails instead
+            // Intent intent = new Intent(VacationList.this, VacationDetails.class);
+            // startActivity(intent);
+            return true;
+        }
+        return true;
     }
 }
