d4e7372e839b414f10f1418cf4a3c7ca8ddb36c9
Deanna H <deannamaharaj@gmail.com>
2025-01-24 16:33:00 -0600

    Add Button on homepage that takes user to the VacationList page

--- app/src/main/java/com/example/vacationplanner/UI/MainActivity.java
+++ app/src/main/java/com/example/vacationplanner/UI/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.vacationplanner.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VacationList.class); // current activity= MainActivity.this; VacationList.class= where we want to go
+                intent.putExtra("test", "Information sent");
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
